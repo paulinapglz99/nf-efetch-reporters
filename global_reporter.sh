0739bc8db330fe99462920c68ab2c0fda529ed52
Alejandra Paulina Pérez González <82723700+paulinapglz99@users.noreply.github.com>
2022-07-22 02:05:38 -0500

    Update global_reporter.sh

--- global_reporter.sh
+++ global_reporter.sh
@@ -1,14 +1,14 @@
 MKSHELL="/bin/bash"
 
-#Contar cuantos outputs de cada tipo hay
-	echo "fasta, $(ls *.fasta | wc -l)" > global_num_fasta.tmp  #busca cuantas hay para cada tipo de output
-	echo "prot_faa, $(ls *-prot.faa | wc -l)" > global_num_protfaa.tmp
-	echo "prot_fna, $(ls *-prot.fna | wc -l)" > global_num_protfna.tmp
-	echo "prot_faa_error, $(ls *-prot.faa.fetcherror | wc -l)" > global_num_protfaaerror.tmp
-	echo "prot_fna_error, $(ls *-prot.fna.fetcherror | wc -l)" > global_num_protfnaerror.tmp
-#juntar los archivos
-	cat *.tmp >> global_report.csv
-#Poner header a la tabla y borrar los archivos temporales
-	sed -i '1i output_type, num' global_report.csv && rm *.tmp
+#This script makes a global report of nf-efecth results
 
-
+#Count outputs of each type
+        echo "fasta, $(ls *.fasta | wc -l)" > global_num_fasta.tmp  #numer of files for each output type
+        echo "prot_faa, $(ls *-prot.faa | wc -l)" > global_num_protfaa.tmp
+        echo "prot_fna, $(ls *-prot.fna | wc -l)" > global_num_protfna.tmp
+        echo "prot_faa_error, $(ls *-prot.faa.fetcherror | wc -l)" > global_num_protfaaerror.tmp
+        echo "prot_fna_error, $(ls *-prot.fna.fetcherror | wc -l)" > global_num_protfnaerror.tmp
+#merge tmp files    
+        cat *.tmp >> global_report.csv
+#Set table header and delete tmp files 
+        sed -i '1i output_type, num' global_report.csv && rm *.tmp
